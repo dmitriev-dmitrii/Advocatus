1b27bdddd05255534da318e51a5f5ecbcfa3292d
Dmitriev-Dmitrii <dmitrievdmitrii@mail.com>
2020-12-25 01:44:15 +0300

    акордеон js
    акоодеон js

---
+++ main.js
@@ -0,0 +1,20 @@
+const cardQestion= document.querySelectorAll('.section-6__content-card-question');
+
+cardQestion.forEach(e => {
+
+e.addEventListener('click',openAnswer)
+
+
+
+});
+
+
+
+function openAnswer() 
+{
+
+
+this.nextElementSibling.classList.toggle('section-6__content-card-answer--active');
+
+this.classList.toggle('fa-minus-square');
+}
