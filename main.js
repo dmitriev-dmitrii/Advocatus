91fea102fda3cba2936c1a2665b77cf47b653ffb
Dmitriev-Dmitrii <dmitrievdmitrii@mail.com>
2021-03-13 21:31:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 
-
 const cardQestion= document.querySelectorAll('.section-6__content-card-question');
 
 cardQestion.forEach(e => {
 
 e.addEventListener('click',openAnswer)
 
-
-
 });
 
 
 function openAnswer() 
 {
-
 
 this.nextElementSibling.classList.toggle('section-6__content-card-answer--active');
 
